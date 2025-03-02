49cbd315df1563f0c95c52ba2b37974cc86d4963
Christina Vieau <cvieau@live.com>
2025-03-01 21:40:27 -0500

    added final data models

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -1,3 +1,25 @@
+using Microsoft.Win32;
 using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bangazon.Models;
+
+public class Seller
+{
+    [Required]
+    public string UserUid { get; set; }
+    [Required]
+    public string SellerUserName { get; set; }
+    public string ImageUrl { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    public string City { get; set; }
+    public string StateOrCountry { get; set; }
+    public string ProductId {  get; set; }
+    public Boolean ProductsSold {  get; set; }
+
+    public User User { get; set; }
+    public List<Product> Products { get; set; }
